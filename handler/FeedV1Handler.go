8791e0c3be077b4c0612f78969fdfcf9fc0dd055
waynelee-lwc <1418551940@qq.com>
2023-03-27 17:00:03 +0800

    handler

---
+++ handler/FeedV1Handler.go
@@ -0,0 +1,12 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/jlu-cow-studio/common/model/http_struct/feed"
+)
+
+func (h *Handler) FeedV1(ctx context.Context, req *feed.GetFeedReq) (res *feed.GetFeedRes, err error) {
+
+	return
+}
